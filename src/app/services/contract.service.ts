832d796e21debe4a911bd4ddcef81e8b15488cb5
Dmytro Nemoga <dnemoga@gmail.com>
2018-12-11 16:37:52 +0200

    Requests through CORS proxy

--- src/app/services/contract.service.ts
+++ src/app/services/contract.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { concatMap, toArray } from 'rxjs/operators';
+import { concatMap, map, take, toArray } from 'rxjs/operators';
 
 @Injectable()
 export class ContractService {
@@ -10,14 +10,11 @@ export class ContractService {
   ) {}
 
   public getContracts(): Observable<any[]> {
-    return this.$httpClient.get<any>('/api/v2/disposers/contracts', {
-      params: {
-        disposerId: '25901106',
-        contractorId: '30731879'
-      }
-    }).pipe(
-      concatMap(({ documents }) => of(...documents)),
-      concatMap(({ id }) => this.$httpClient.get<any>(`/api/v2/disposers/contracts/${id}`)),
+    return this.$httpClient.get<any>('https://cors-proxy.htmldriven.com/?url=http://api.spending.gov.ua/api/v2/disposers/contracts?disposerId=25901106&contractorId=30731879').pipe(
+      map(({ body }) => JSON.parse(body)),
+      concatMap(({ documents }) => of(...documents).pipe(take(10))),
+      concatMap(({ id }) => this.$httpClient.get<any>(`https://cors-proxy.htmldriven.com/?url=http://api.spending.gov.ua/api/v2/disposers/contracts/${id}`)),
+      map(({ body }) => JSON.parse(body)),
       toArray()
     );
   }
